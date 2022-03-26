2596818eed14aab5159c239de89bada70de86c5c
Fmahboob <91558787+Fmahboob@users.noreply.github.com>
2022-03-25 23:13:50 -0400

    Added lists of items in recycler view

--- app/src/main/java/com/example/cookingcorner/recipeFragment.java
+++ app/src/main/java/com/example/cookingcorner/recipeFragment.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.cookingcorner.RecipeRecyclerView.CustomRecipeAdopter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,12 +63,20 @@ public class recipeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+//Created Array list and adopter 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false);
+       View view = inflater.inflate(R.layout.fragment_recipe, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recipeList);
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe("Pasta", R.drawable.ic_launcher_background));
+        CustomRecipeAdopter adopter = new CustomRecipeAdopter(recipes);
+        recyclerView.setAdapter(adopter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+       return view;
     }
 
 
