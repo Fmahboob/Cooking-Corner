0ed9d4b212b486b6dabd6d229c99313c38ac0fa4
Farina <W0766260@myscc.ca>
2022-04-19 10:35:23 -0400

    Did About Us

--- app/src/main/java/com/example/cookingcorner/MainActivity.java
+++ app/src/main/java/com/example/cookingcorner/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_credit, R.id.nav_shop, R.id.nav_recipe,
+                R.id.nav_about, R.id.nav_credit, R.id.nav_shop, R.id.nav_recipe,
                 R.id.nav_tips)
                 .setOpenableLayout(drawer)
                 .build();
